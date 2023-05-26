91c359c65eecb1aa4128031e17160c86a7fdd5e3
Thuy Quang <thuyqnph35609@fpt.edu.vn>
2023-05-26 14:24:24 +0700

    change some texts

--- app/src/main/java/com/thuydev/asm1/login_layout.java
+++ app/src/main/java/com/thuydev/asm1/login_layout.java
@@ -15,7 +15,8 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class login_layout extends AppCompatActivity {
-
+    String tenNguoi=null;
+    String matKhau = null;
     ActivityResultLauncher<Intent> getData = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
@@ -26,9 +27,10 @@ public class login_layout extends AppCompatActivity {
                         Bundle bundle = intent.getExtras();
                         EditText uSer = findViewById(R.id.txt_user);
                         EditText pAss = findViewById(R.id.txt_pass);
-
-                        uSer.setText(bundle.getString("User1"));
-                        pAss.setText(bundle.getString("Pass1"));
+                        tenNguoi = bundle.getString("User1");
+                        matKhau = bundle.getString("Pass1");
+                        uSer.setText(tenNguoi);
+                        pAss.setText(matKhau);
                     }
                 }
             }
@@ -43,20 +45,46 @@ public class login_layout extends AppCompatActivity {
         setContentView(R.layout.login_layout);
         Button bnt = findViewById(R.id.btn_nut1);
         Button bnt2 = findViewById(R.id.btn_nut2);
+//        EditText name = findViewById(R.id.txt_user);
+//        EditText Pass = findViewById(R.id.txt_pass);
+//        bnt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(login_layout.this, sign_in_layout.class);
+//                getData.launch(intent);
+//            }
+//        });
 
+
+        EditText name = findViewById(R.id.txt_user);
+        EditText Pass = findViewById(R.id.txt_pass);
         bnt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(login_layout.this, sign_in_layout.class);
+                Intent intent = new Intent(login_layout.this , sign_in_layout.class);
                 getData.launch(intent);
             }
         });
-
         bnt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean check = false;
+                String sName = name.getText().toString();
+                String sPass = Pass.getText().toString();
                 Intent intent = new Intent(login_layout.this,main_layout.class);
-                startActivity(intent);
+                if(sName.equals(tenNguoi)){
+                    if (sPass.equals(matKhau)){
+                        check= true;
+                        startActivity(intent);
+
+                    }
+                }
+                if(check==false){
+                    Toast.makeText(login_layout.this,"Sai tên đăng nhập hoặc mật khẩu",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(login_layout.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
