895f109495cf4364b90f4e4846d681209a7c94b8
Thuy Quang <thuyqnph35609@fpt.edu.vn>
2023-06-13 09:26:53 +0700

    update

---
+++ app/src/main/java/com/thuydev/asm1/PhongBan.java
@@ -0,0 +1,14 @@
+package com.thuydev.asm1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PhongBan extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vp_layout);
+    }
+}
--- app/src/main/java/com/thuydev/asm1/main_layout.java
+++ app/src/main/java/com/thuydev/asm1/main_layout.java
@@ -1,6 +1,9 @@
 package com.thuydev.asm1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +13,30 @@ public class main_layout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_app);
-        
+        Button pb = findViewById(R.id.btn_pb);
+        Button nv = findViewById(R.id.btn_nv);
+        Button exit = findViewById(R.id.btn_exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        pb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_layout.this,PhongBan.class);
+                startActivity(intent);
+            }
+        });
+        nv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_layout.this,NhanVien.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 }
